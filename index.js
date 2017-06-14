d4bcb20928ad03f3bafa6241872b4fdc08461f77
smitch88 <scottymitch88@gmail.com>
2017-06-14 13:21:04 -0400

    Add some faker in development mode just to spruce it up

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const amqplib = require('amqplib');
 
 const isDevelopment = process.env.NODE_ENV !== "production";
+
 const q = 'queue';
 
 // Initialize connection to rabbitmq w/ amqp
 const connection = amqplib.connect('amqp://' + process.env.URL);
-
-// Producer
-connection
-  .then((conn) => conn.createChannel())
-  .then((ch) => (
-    ch.assertQueue(q)
-      .then((ok) => ch.sendToQueue(q, new Buffer('ALL THE THINGS!')))
-  ))
-  .catch(console.warn);
+const serialize = JSON.stringify;
+const deserialize = JSON.parse;
 
 // Consumer
 connection
@@ -22,11 +16,42 @@ connection
     ch.assertQueue(q)
       .then((ok) => (
         ch.consume(q, (msg) => {
-          if (msg !== null) {
-            console.log('==== Received shiz from the wire: ', msg.content.toString());
-            ch.ack(msg);
+          try {
+            if (msg !== null) {
+              console.log('==== Received shiz: ', serialize(deserialize(msg.content)));
+              ch.ack(msg);
+            }
+          } catch(e){
+            console.error('==== Error in consumer process.', e.message);
           }
         })
     )
   )))
   .catch(console.warn);
+
+/*
+* Development mock out producer sending out serializable datas
+*/
+
+if(isDevelopment){
+  const faker = require('faker');
+  const config = {
+    interval: 1500,
+    interpolation: () => ({
+      name: faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar()
+    })
+  };
+  connection
+    .then((conn) => conn.createChannel())
+    .then((ch) => (
+      ch.assertQueue(q)
+        .then((ok) => (
+          setInterval(() => (
+            ch.sendToQueue(q, new Buffer(serialize(config.interpolation())))
+          ), config.interval)
+        ))
+    ))
+    .catch(console.warn);
+}
